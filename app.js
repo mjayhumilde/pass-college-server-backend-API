b5d12f57ad268333a30b8cc16da7f9bc20839b64
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-06-11 10:03:56 +0800

    install slugify and created a model, controller and route folder. Completed the Model Schema for Post

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
+// START EXPRESS APP
 const app = express();
 
-// Middleware
+// 1) Global Middleware
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Enable parsing of JSON request bodies
 
-// Simple test route
-app.get("/", (req, res) => {
-  res.send("API is running!");
+// 2) ROUTES
+app.get("/api/v1/post", (req, res) => {
+  res.send("Get All Post Types");
+});
+app.get("/api/v1/request", (req, res) => {
+  res.send("Get All Request Documents");
 });
 
 module.exports = app; // Export the app instance
---
+++ routes/postRoute.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+// Create
+router.route("/").post(postController);
+
+// Read
+router.route("/annoucement").get(postController);
+router.route("/news").get(postController);
+router.route("/events").get(postController);
+router.route("/uniforms-update").get(postController);
+router.route("/careers").get(postController);
+
+// Update
+router.route("/annoucement:id").patch(postController);
+router.route("/news:id").patch(postController);
+router.route("/events:id").patch(postController);
+router.route("/uniforms-update:id").patch(postController);
+router.route("/careers:id").patch(postController);
+
+// Delete
+router.route("/annoucement:id").delete(postController);
+router.route("/news:id").delete(postController);
+router.route("/events:id").delete(postController);
+router.route("/uniforms-update:id").delete(postController);
+router.route("/careers:id").delete(postController);
+
+module.exports = router;
