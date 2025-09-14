7037e6f0ac73f416a0397030f1f035d7433ad8c5
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-09-14 14:52:19 +0800

    adjust the authController protection to not block the getAllComment midleware and changed the populated fields to firstName lastName photo and course in commentController

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -3,18 +3,15 @@ const router = express.Router();
 const commentController = require("../controller/commentController");
 const authController = require("../controller/authController");
 
-// Protect all routes after this
-router.use(authController.protect);
-
 router
   .route("/")
   .get(commentController.filterCommentsByPost, commentController.getAllComments)
-  .post(commentController.createComment);
+  .post(authController.protect, commentController.createComment);
 
 router
   .route("/:id")
   .get(commentController.getOneComment)
-  .patch(commentController.updateComment)
-  .delete(commentController.deleteComment);
+  .patch(authController.protect, commentController.updateComment)
+  .delete(authController.protect, commentController.deleteComment);
 
 module.exports = router;
