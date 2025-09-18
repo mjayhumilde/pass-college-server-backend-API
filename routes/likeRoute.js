cc9e6005dab422872f0d17037735f02cec8d7c8a
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-09-18 09:35:53 +0800

    adjust the authController protection to not block the getAllLikes midleware

--- routes/likeRoute.js
+++ routes/likeRoute.js
@@ -3,17 +3,14 @@ const router = express.Router();
 const likeController = require("../controller/likeController");
 const authController = require("../controller/authController");
 
-// Protect all routes after this
-router.use(authController.protect);
-
 router
   .route("/")
   .get(likeController.getAllLikes)
-  .post(likeController.createLike);
+  .post(authController.protect, likeController.createLike);
 
 router
   .route("/:id")
   .get(likeController.getOneLike)
-  .delete(likeController.deleteLike);
+  .delete(authController.protect, likeController.deleteLike);
 
 module.exports = router;
