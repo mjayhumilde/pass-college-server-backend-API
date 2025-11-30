1665adfd283103c7e9f5ae7eb4a60326361ffe7b
Mark John Humilde <markjohnhumilde14@gmail.com>
2025-11-30 15:52:35 +0800

    refractor return document structure for documentReport endpoints.=

--- controller/documentReportController.js
+++ controller/documentReportController.js
@@ -3,6 +3,14 @@ const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const { getDateRange } = require("../utils/timerange");
 
+const defaultDocumentBreakdown = {
+  goodMoral: 0,
+  idCard: 0,
+  grades: 0,
+  diploma: 0,
+  transcriptOfRecords: 0,
+};
+
 exports.getSummaryReport = catchAsync(async (req, res, next) => {
   const { range } = req.query;
 
@@ -14,25 +22,35 @@ exports.getSummaryReport = catchAsync(async (req, res, next) => {
     createdAt: { $gte: start, $lte: end },
   });
 
-  const summary = {
-    timeframe: range,
-    totalRequests: docs.length,
-    statusBreakdown: {
-      pending: docs.filter((d) => d.documentStatus === "pending").length,
-      processing: docs.filter((d) => d.documentStatus === "processing").length,
-      readyToPickup: docs.filter((d) => d.documentStatus === "ready-to-pickup")
-        .length,
-      completed: docs.filter((d) => d.documentStatus === "completed").length,
-      cancelled: docs.filter((d) => d.documentStatus === "cancelled").length,
-    },
+  // status summary
+  const statusBreakdown = {
+    pending: docs.filter((d) => d.documentStatus === "pending").length,
+    processing: docs.filter((d) => d.documentStatus === "processing").length,
+    readyToPickup: docs.filter((d) => d.documentStatus === "ready-to-pickup")
+      .length,
+    completed: docs.filter((d) => d.documentStatus === "completed").length,
+    cancelled: docs.filter((d) => d.documentStatus === "cancelled").length,
   };
+
+  // document type summary
+  let documentBreakdown = { ...defaultDocumentBreakdown };
+
+  docs.forEach((doc) => {
+    if (documentBreakdown[doc.documentType] !== undefined) {
+      documentBreakdown[doc.documentType]++;
+    }
+  });
 
   res.status(200).json({
     status: "success",
-    data: summary,
+    data: {
+      timeframe: range,
+      totalRequests: docs.length,
+      statusBreakdown,
+      documentBreakdown,
+    },
   });
 });
-
 exports.getTypeReport = catchAsync(async (req, res, next) => {
   const { range } = req.query;
 
